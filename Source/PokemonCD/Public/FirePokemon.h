c239f2011150b2c954a9f12f75fda1dc0710023f
LazarusKim <devlazarus729@gmail.com>
2024-02-22 20:18:13 +0900

    Add: VFX에셋 추가, 스킬 스폰 구현중

---
+++ Source/PokemonCD/Public/FirePokemon.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FirePokemon.generated.h"
+
+UCLASS()
+class POKEMONCD_API AFirePokemon : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFirePokemon();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent* BoxComponent;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UArrowComponent* SkillSpawnPos;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	class UParticleSystem* SkillParticle;
+
+	UPROPERTY(EditDefaultsOnly)
+	class AEnemy* Enemy;
+
+	FTimerHandle Timer;
+	
+	void FindActor();
+	void SpawnParticle();
+};
